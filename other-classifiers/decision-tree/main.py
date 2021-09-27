07a3b02720f24aff27b76d31180746d469eee432
DelmerLopez <delmrlophz@gmail.com>
2021-09-27 12:18:25 -0500

    adding macro metrics to decision tree

--- other-classifiers/decision-tree/main.py
+++ other-classifiers/decision-tree/main.py
@@ -1,9 +1,14 @@
 import csv
+from posixpath import join
 import numpy as np
 import graphviz
 import pickle
+import scikitplot
+import os
+from matplotlib import pyplot as plt
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import StratifiedKFold
 
 x_train = pickle.load(open('../../X_train.pickle', 'rb'))
@@ -16,14 +21,39 @@ y = np.concatenate((y_train, y_test), axis=None)
 kf = StratifiedKFold(n_splits=10)
 n_fold = 1
 accuracies = []
+precisions = []
+recalls = []
+f1_scores = []
+
+cm_folder = 'confusion-matrix'
+if not os.path.exists(cm_folder):
+    os.makedirs(cm_folder)
 
 for train_index, test_index in kf.split(x, y):
     model = DecisionTreeClassifier()
     model.fit(x[train_index],y[train_index])
-    score = model.score(x[test_index], y[test_index])
-    print("Fold: {} Accuraccy: {}".format(n_fold, score))
-    accuracies.append(score)
+    y_pred = model.predict(x[test_index])
 
+    accuracy = accuracy_score(y[test_index], y_pred)
+    precision = precision_score(y[test_index], y_pred, average='macro', zero_division=0)
+    recall = recall_score(y[test_index], y_pred, average='macro', zero_division=0)
+    f1 = f1_score(y[test_index], y_pred, average='macro', zero_division=0)
+
+    accuracies.append(accuracy)
+    precisions.append(precision)
+    recalls.append(recall)
+    f1_scores.append(f1)
+
+    print("Fold: {} Accuraccy: {}".format(n_fold, accuracy))
+
+    scikitplot.metrics.plot_confusion_matrix(y[test_index], 
+                                            y_pred,
+                                            title='Decision Tree Confusion Matrix', 
+                                            normalize=True, text_fontsize='small')
+    plt.savefig(join(cm_folder, "{}-cm".format(n_fold)))
+    plt.clf()
+    plt.close()
+    
     dot_data = tree.export_graphviz(model, out_file=None)
     graph = graphviz.Source(dot_data)
     graph.render("{}-dt-result".format(n_fold))
@@ -32,11 +62,15 @@ for train_index, test_index in kf.split(x, y):
 with open('dt-results.csv', 'w', encoding='UTF-8', newline='') as csvfile:
     writer = csv.writer(csvfile)
     fold = 1
-    header = ['fold', 'precision']
+    header = ['fold', 'accuracy', 'precision', 'recall', 'f1_score']
     writer.writerow(header)
-    for accuracy in accuracies:
-        row = [fold, accuracy]
+    for accuracy, precision, recall, f1 in zip(accuracies, precisions, recalls, f1_scores):
+        row = [fold, accuracy, precision, recall, f1]
         writer.writerow(row)
         fold += 1
-    avgs = ["Average", np.mean(np.array(accuracies))]
+    avgs = ["Average", 
+            np.mean(np.array(accuracies)),
+            np.mean(np.array(precisions)),
+            np.mean(np.array(recalls)),
+            np.mean(np.array(f1_scores))]
     writer.writerow(avgs)
